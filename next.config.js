208bb22ff36bf0a512abe90024432c633fb45afa
Emil Sonesson <emil.sonesson@noend.com>
2021-12-01 22:52:31 +0100

    Add i18n config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const withTM = require("@vercel/edge-functions-ui/transpile")()
 
 module.exports = withTM({
-  // i18n: {
-  //   locales: ["sv", "en"],
-  //   defaultLocale: "sv",
-  // },
+  i18n: {
+    locales: ["sv", "en"],
+    defaultLocale: "sv",
+  },
 })
